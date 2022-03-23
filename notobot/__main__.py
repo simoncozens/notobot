b326ef870d59d4ffe8c32edaf550e5d378b0c650
Simon Cozens <simon@simon-cozens.org>
2022-03-23 15:46:43 +0000

    Use GitHub API instead

--- notobot/__main__.py
+++ notobot/__main__.py
@@ -1,6 +1,8 @@
 import os
 
 from aiohttp import web
+from aiostream import stream, pipe
+
 import cairosvg
 import pygit2
 from fontv.libfv import FontVersion
@@ -11,93 +13,33 @@ from PIL import Image, ImageOps
 import cloudinary.uploader
 import re
 import aiohttp
+import base64
 import threading
 
 from gidgethub import routing, sansio
 from gidgethub import aiohttp as gh_aiohttp
+import asyncio
 
 router = routing.Router()
 username = "simoncozens"
 
-
-class MyRemoteCallbacks(pygit2.RemoteCallbacks):
-    def transfer_progress(self, stats):
-        if stats.indexed_objects % 100 == 0:
-            print(f"{stats.indexed_objects}/{stats.total_objects}")
+secret = os.environ.get("GH_SECRET")
+oauth_token = os.environ.get("GH_AUTH")
 
 
-def clone_repo():
-    print("Checking out Noto repo")
-    pygit2.clone_repository(
-        "https://github.com/googlefonts/noto-fonts",
-        "notofonts",
-        callbacks=MyRemoteCallbacks(),
-    )
+async def shape_this_blob(string, blob, commit):
+    with tempfile.NamedTemporaryFile() as ntf:
+        ntf.write(blob)
+        vh = Vharfbuzz(ntf.name)
+        buf = vh.shape(string)
+        serialized = vh.serialize_buf(buf)
+        s = {
+            "commit": commit,
+            "version": FontVersion(ntf.name).get_version_number_string(),
+            "shaping": serialized,
+            "svg": vh.buf_to_svg(buf),
+        }
 
-
-if not os.path.isdir("notofonts"):
-    download_thread = threading.Thread(target=clone_repo, name="Clone")
-    download_thread.start()
-
-
-def shape_all_versions(path, string):
-    def shape_this_blob(versions, blob, commit):
-        with tempfile.NamedTemporaryFile() as ntf:
-            ntf.write(blob)
-            vh = Vharfbuzz(ntf.name)
-            buf = vh.shape(string)
-            serialized = vh.serialize_buf(buf)
-            versions.append(
-                {
-                    "commit": commit,
-                    "version": FontVersion(ntf.name).get_version_number_string(),
-                    "shaping": serialized,
-                    "svg": vh.buf_to_svg(buf),
-                }
-            )
-
-    repo = pygit2.Repository(os.path.join(os.getcwd(), "notofonts", ".git"))
-    prev = None
-    oldversion = ""
-    versions = []
-    try:
-        for cur in repo.walk(repo.head.target):
-            if prev is not None:
-                commit = prev.id
-                diff = cur.tree.diff_to_tree(prev.tree)
-                for patch in diff:
-                    if (
-                        path in patch.delta.new_file.path
-                        and "unhinted/ttf" in patch.delta.new_file.path
-                    ):
-                        blob = cur.tree[patch.delta.new_file.path].data
-                        if blob == oldversion:
-                            continue
-                        shape_this_blob(versions, blob, commit)
-                        oldversion = blob
-            prev = cur
-            if cur.parents:
-                cur = cur.parents[0]
-    except Exception as e:
-        print(e)
-
-    return versions
-
-
-def answer_question(question):
-    if not "@notobot" in question:
-        return
-    m = re.search("regression test (.*) with (.*)", question)
-    if not m:
-        return
-    string, file = m[1], m[2]
-
-    shaped = shape_all_versions(file, string)
-    if not shaped:
-        return
-    message = "Here's your regression log:\n\n"
-
-    for ix, s in enumerate(shaped):
         svg = s["svg"].replace('transform="matrix(1 0 0 -1 0 0)"', "")
         png = cairosvg.svg2png(bytestring=svg)
         image = Image.open(io.BytesIO(png))
@@ -107,58 +49,132 @@ def answer_question(question):
         new_image = Image.new("RGBA", cropped.size, "WHITE")
         new_image.paste(cropped, (0, 0), cropped)
         new_image.thumbnail((600, 400), Image.ANTIALIAS)
-        new_image.save("img-%i.png" % ix)
-        upload = cloudinary.uploader.upload("img-%i.png" % ix)
-        s["url"] = upload["url"]
-        message += f"## {file} {s['version']} @ {s['commit'].hex[0:7]}\n\n"
+        img_byte_arr = io.BytesIO()
+        new_image.save(img_byte_arr, format="PNG")
+        # upload = cloudinary.uploader.upload(img_byte_arr.getvalue())
+        # s["url"] = upload["url"]
+    return s
+
+
+async def all_versions(gh, path, text):
+    shas = [
+        (text, path, x["sha"])
+        for x in await gh.getitem(
+            "https://api.github.com/repos/googlefonts/noto-fonts/commits?path=" + path
+        )
+    ]
+    if len(shas) > 10:
+        shas = shas[0:10]
+    ghs = stream.repeat(gh)
+    xs = stream.zip(ghs, stream.iterate(shas))
+    ys = stream.starmap(xs, get_version, ordered=True)
+    return await stream.list(stream.starmap(ys, shape_this_blob))
+
+
+async def get_version(gh, pair):
+    text, path, version = pair
+    cache_key = "/tmp/cache_" + path.replace("/", "_") + "-" + version
+    if os.path.isfile(cache_key):
+        return text, open(cache_key, "rb").read(), version
+    print(f"Downloading {path}@{version}")
+    default_tree = await gh.getitem(
+        f"https://api.github.com/repos/googlefonts/noto-fonts/git/trees/{version}?recursive=1"
+    )
+    tree_entry = [t for t in default_tree["tree"] if t["path"] == path][0]
+    data = (await gh.getitem(tree_entry["url"]))["content"]
+    binary = base64.b64decode(data)
+    with open(cache_key, "wb") as f:
+        f.write(binary)
+    return text, binary, version
+
+
+async def answer_question(gh, question):
+    if not "@notobot" in question:
+        return
+    m = re.search("regression test (.*) with (.*)", question)
+    if not m:
+        return
+    string, file = m[1], m[2]
+
+    if file.startswith("/"):
+        file = file[1:]
+    if "hinted" not in file:
+        file = "unhinted/ttf/" + file
+
+    print("Getting %s" % file)
+    shaped = await all_versions(gh, file, string)
+    if not shaped:
+        return
+    message = "Here's your regression log:\n\n"
+
+    for ix, s in enumerate(shaped):
+        message += f"## {file} {s['version']} @ {s['commit']}\n\n"
         message += f"`{s['shaping']}`\n\n"
-        message += f"<img src=\"{s['url']}\">\n\n"
+        if "url" in s:
+            message += f"<img src=\"{s['url']}\">\n\n"
     return message
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, username, oauth_token=oauth_token)
+        print(
+            await answer_question(
+                gh,
+                "@notobot, regression test سبے with NotoNastaliqUrdu/NotoNastaliqUrdu-Regular.ttf",
+            )
+        )
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 
 # print(
 #     answer_question("@notobot, regression test سبے with NotoNastaliqUrdu-Regular.ttf")
 # )
 
-routes = web.RouteTableDef()
+# routes = web.RouteTableDef()
 
 
-@router.register("issue_comment", action="created")
-async def issue_comment_event(event, gh, *args, **kwargs):
-    url = event.data["issue"]["comments_url"]
-    message = answer_question(event.data["comment"]["body"])
-    if message:
-        await gh.post(url, data={"body": message})
-    pass
+# @router.register("issue_comment", action="created")
+# async def issue_comment_event(event, gh, *args, **kwargs):
+#     print("Issue comment created")
+#     print(event.data)
+#     url = event.data["issue"]["comments_url"]
+#     message = answer_question(event.data["comment"]["body"])
+#     if message:
+#         await gh.post(url, data={"body": message})
+#     pass
 
 
-@routes.post("/")
-async def main(request):
-    # read the GitHub webhook payload
-    body = await request.read()
+# @routes.post("/")
+# async def main(request):
+#     # read the GitHub webhook payload
+#     body = await request.read()
 
-    # our authentication token and secret
-    secret = os.environ.get("GH_SECRET")
-    oauth_token = os.environ.get("GH_AUTH")
+#     # our authentication token and secret
+#     secret = os.environ.get("GH_SECRET")
+#     oauth_token = os.environ.get("GH_AUTH")
 
-    # a representation of GitHub webhook event
-    event = sansio.Event.from_http(request.headers, body, secret=secret)
+#     # a representation of GitHub webhook event
+#     event = sansio.Event.from_http(request.headers, body, secret=secret)
 
-    async with aiohttp.ClientSession() as session:
-        gh = gh_aiohttp.GitHubAPI(session, username, oauth_token=oauth_token)
+#     async with aiohttp.ClientSession() as session:
+#         gh = gh_aiohttp.GitHubAPI(session, username, oauth_token=oauth_token)
 
-        # call the appropriate callback for the event
-        await router.dispatch(event, gh)
+#         # call the appropriate callback for the event
+#         await router.dispatch(event, gh)
 
-    # return a "Success"
-    return web.Response(status=200)
+#     # return a "Success"
+#     return web.Response(status=200)
 
 
-if __name__ == "__main__":
-    app = web.Application()
-    app.add_routes(routes)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
+# if __name__ == "__main__":
+#     app = web.Application()
+#     app.add_routes(routes)
+#     port = os.environ.get("PORT")
+#     if port is not None:
+#         port = int(port)
 
-    web.run_app(app, port=port)
+#     web.run_app(app, port=port)
